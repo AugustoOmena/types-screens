2bae62f4e7b1b3cb10e8b8cdfff517e919f6cbcd
Augusto Omena <122471298+AugustoOmena@users.noreply.github.com>
2024-08-08 23:14:46 -0300

    Ajuste na posição do botão e array de alarmes

--- src/views/Onboarding/First.tsx
+++ src/views/Onboarding/First.tsx
@@ -1,18 +1,42 @@
-import { Text, TouchableOpacity, View } from "react-native";
-import React from "react";
+import { Text, TouchableOpacity, View, FlatList } from "react-native";
+import React, { useState } from "react";
 import { styles } from "../../../styles";
 import { useAppNavigation } from "../../utils/useAppNavigation";
 import CalendarioNoTopo from "./Calendario";
 
+type Alarme = {
+    hora: string;
+    nome: string;
+};
+
 const First = () => {
+
     const navigation = useAppNavigation();
+    const [alarmes, setAlarmes] = useState<Alarme[]>([
+        { hora: '08:00 AM', nome: 'Acordar' },
+        { hora: '12:00 PM', nome: 'Almoço' },
+        { hora: '07:00 PM', nome: 'Exercício' },
+    ]);
+
+    const renderItem = ({ item }: { item: Alarme }) => (
+        <View >
+            <Text >Hora: {item.hora}</Text>
+            <Text >Nome: {item.nome}</Text>
+        </View>
+    );
+
     return (
-        <View>
-            <CalendarioNoTopo></CalendarioNoTopo>
-            <View style={styles.container}>
-                <Text>First</Text>
+        <View style={styles.fullScreenContainer}>
+            <CalendarioNoTopo />
 
-
+            <FlatList
+                data={alarmes}
+                renderItem={renderItem}
+                keyExtractor={(item, index) => index.toString()}
+                contentContainerStyle={styles.listContainer}
+            />
+            
+            <View style={styles.Bottom}>
                 <TouchableOpacity
                     onPress={() =>
                         navigation.navigate("Onboarding", {
--- styles.ts
+++ styles.ts
@@ -1,6 +1,13 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
+  fullScreenContainer: {
+    flex: 1,
+  },
+  listContainer: {
+      paddingBottom: 525,
+      paddingHorizontal: 20,
+  },
   container: {
     top: 170,
     justifyContent: "center",
@@ -8,17 +15,23 @@ export const styles = StyleSheet.create({
   },
   button: {
     backgroundColor: 'white',
-    borderRadius: 47,
-    width: 90,
-    height: 90,
-    justifyContent: 'center',
-    alignItems: 'center',
-    margin: 20,
-    bottom: -600
+        borderRadius: 47,
+        width: 90,
+        height: 90,
+        justifyContent: 'center',
+        alignItems: 'center',
   },
   buttonText: {
-      textAlign: 'center',
-      fontSize: 50,
+    textAlign: 'center',
+    fontSize: 50,
   },
+  Bottom: {
+    position: 'absolute',
+        bottom: 20,
+        left: 0,
+        right: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+  }
   });
   
